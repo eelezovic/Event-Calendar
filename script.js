24bd40d71ce65ba5abcc1695ecbd8f861b6633fd
Emir <elitedesire17@gmail.com>
2022-08-23 21:00:41 -0400

    newest code

---
+++ script.js
@@ -0,0 +1,18 @@
+const date = new Date();
+
+const months = [
+    "Januart",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+document.querySelector("").innerHTML
