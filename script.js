cc5753293abf9ee746b4aa93fae01304ed4bda34
Emir <elitedesire17@gmail.com>
2022-08-29 22:27:52 -0400

    new changes

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
- const date = new Date();
+const date = new Date();
 
- const calander = document.querySelector(".days");
 
- const month = date.getMonth();
- console.log(month);
- const months  = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-    ];
+
+//current day and month
+const month = date.getMonth();
+const months  = [
+   "January",
+   "February",
+   "March",
+   "April",
+   "May",
+   "June",
+   "July",
+   "August",
+   "September",
+   "October",
+   "November",
+   "December",
+   ];
 
 document.querySelector(".date h1").innerHTML
 = months [date.getMonth()];
 
 document.querySelector(".date p").innerHTML
-=date.toDateString();
+= date.toDateString();
 
-
-
-
-
-
-// document.querySelector("").innerHTML
-// =month.toDateString();
-const nextButton =document.querySelector(".next");
-
-//nextButton.addEventListener('click', () => {
- //   alert(".nextBotton")});
