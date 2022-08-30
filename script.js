1a70e07481902bc7282db13c5692cf64666c010a
Emir <elitedesire17@gmail.com>
2022-08-30 00:42:10 -0400

    new repo

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+// Creating drag and drop options for the icons
+const draggableElements = document.querySelectorAll(".icons");
+const droppableElements = document.querySelector(".days")
+
+draggableElements.forEach(elem => {
+    elem.addEventListener("dragstart", dragStart)
+});
+
+droppableElements.forEach(elem => {
+    elem.addEventListener("dragover", dragOver);
+    elem.addEventListener("drop", drop);
+});
+
+
+function dragOver(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+}
+
+
+
+// Current date 
 const date = new Date();
-
-
-
 //current day and month
 const month = date.getMonth();
 const months  = [
