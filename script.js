b38556b27db57cf7aadc5ede2fa5603ff708f160
Emir <elitedesire17@gmail.com>
2022-09-01 00:04:57 -0400

    Latest Repo

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-// Creating drag and drop options for the icons
-const draggableElements = document.querySelectorAll(".icons");
-const droppableElements = document.querySelector(".days")
+// Drag and Drop
+// Creating drag and drop options for the icons (fill= calendar icons, empties= days)
+const fill = document.querySelectorAll(".icons");
+const empties = document.querySelectorAll(".days");
 
-draggableElements.forEach(elem => {
-    elem.addEventListener("dragstart", dragStart)
-});
+// Callendar Icons "Fill"
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
 
-droppableElements.forEach(elem => {
-    elem.addEventListener("dragover", dragOver);
-    elem.addEventListener("drop", drop);
-});
+//Loop throught "emoty" days and call drag events
+for(const days of empties) {
+    days.addEventListener("dragenter", dragEnter);
+    days.addEventListener("dragenter", dragEnter);
+    days.addEventListener("dragleave", dragLeave);
+    days.addEventListener("drop", dragDrop);
+}
+// Drag Functions
+function dragStart(events) {
+    this.className += "hold";
+    setTimeout(() => (this.className = "hide"), 0);
+}
+function dragEnd(events) {
+    this.className = "fill";
 
-
-function dragOver(event) {
-    event.preventDefault();
+}
+function dragEnter(events) {
+    events.preventDefault();
+}
+function dragOver(events) {
+    events.preventDefault();
+    this.className += "hovered"
+}
+function dragLeave(events) {
+    this.className = "days";
+}
+function dragDrop(events) {
+    this.className = "days";
+    this.append(fill);
 }
 
-function drop(event) {
-    event.preventDefault();
-}
 
 
-
-// Current date 
+// Setting current date 
 const date = new Date();
 //current day and month
 const month = date.getMonth();
