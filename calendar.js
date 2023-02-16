c3d18b59d3ad304aca2ba4a79b9d77296f587a72
Emir <elitedesire17@gmail.com>
2023-02-16 14:23:06 -0500

    js local storage

--- calendar.js
+++ calendar.js
@@ -64,8 +64,6 @@ const eventsArr = [
 //set an empty array
 let eventsArr = [];
 
-//Calling get events
-getEvents();
 
 // Function to add days
 function initCalendar () {
@@ -388,9 +386,6 @@ function updateEvents(headerMonthandYearDocumentElement) {
              </div>`;
     }
      containerDisplayingEvents.innerHTML = events;
-
-    //Save events when update event called
-    saveEvents();
 }
 
 //Function to add events 
@@ -542,5 +537,5 @@ if (
     if (localStorage.getItem("events" === null)) {
         return;
     }
-        eventsArr.push(...JSON.parse(localStorage.getItem("events")));
+        eventsArr.push(JSON.parse(localStorage.getItem("events")));
     };
