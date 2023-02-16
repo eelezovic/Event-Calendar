9e65d272eb69d66b90d88b5362360a9bea9fe5f7
Emir <elitedesire17@gmail.com>
2023-02-16 14:11:40 -0500

    JS local storage

--- calendar.js
+++ calendar.js
@@ -539,7 +539,7 @@ if (
    }
 
   function getEvents() {
-    if (localStorage.getItem("events" === true)) {
+    if (localStorage.getItem("events" === null)) {
         return;
     }
         eventsArr.push(...JSON.parse(localStorage.getItem("events")));
